596327ee6f48f764d45c02b4af0e9a8304fa0ea7
Daniel Jesus <140974618+danielj13sus@users.noreply.github.com>
2025-10-06 20:18:24 -0300

    Movimentando o Cavalo

--- mvm_peca_xadrez.c
+++ mvm_peca_xadrez.c
@@ -3,10 +3,11 @@
 const int PASSOS_TORRE = 5;
 const int PASSOS_BISPO = 5;
 const int PASSOS_RAINHA = 8;
+const int PASSOS_CAVALO_VERTICAL = 2;
+const int PASSOS_CAVALO_HORIZONTAL = 1;
 
 void simularMovimentoTorre() {
     printf("--- Movimento da Torre (%d casas) ---\n", PASSOS_TORRE);
-
     for (int i = 0; i < PASSOS_TORRE; ++i) {
         printf("Direita\n");
     }
@@ -15,8 +16,7 @@ void simularMovimentoTorre() {
 
 void simularMovimentoBispo() {
     printf("--- Movimento do Bispo (%d casas) ---\n", PASSOS_BISPO);
-
-    int contador = 0; 
+    int contador = 0;
     while (contador < PASSOS_BISPO) {
         printf("Cima, Direita\n");
         contador++;
@@ -29,13 +29,33 @@ void simularMovimentoRainha() {
     if (PASSOS_RAINHA <= 0) {
         return;
     }
-
     int contador = 0;
     do {
         printf("Esquerda\n");
         contador++;
     } while (contador < PASSOS_RAINHA);
-    
+    printf("\n");
+}
+
+void simularMovimentoCavalo() {
+    printf("--- Movimento do Cavalo (%d vertical, %d horizontal) ---\n",
+           PASSOS_CAVALO_VERTICAL, PASSOS_CAVALO_HORIZONTAL);
+
+    for (int segmento = 0; segmento < 2; ++segmento) {
+        if (segmento == 0) {
+            int passos_dados = 0;
+            while (passos_dados < PASSOS_CAVALO_VERTICAL) {
+                printf("Baixo\n");
+                passos_dados++;
+            }
+        } else {
+            int passos_dados = 0;
+            while (passos_dados < PASSOS_CAVALO_HORIZONTAL) {
+                printf("Esquerda\n");
+                passos_dados++;
+            }
+        }
+    }
     printf("\n");
 }
 
@@ -43,6 +63,7 @@ int main() {
     simularMovimentoTorre();
     simularMovimentoBispo();
     simularMovimentoRainha();
+    simularMovimentoCavalo();
 
     return 0;
 }
